f3ef2479d5f3a70b3c6d2df06ae5ae146fc66673
Tim Winchester <tim_sama@hotmail.com>
2018-01-08 20:49:00 -0800

    Initial commit
    Just got rid of last shell script usage, so everything is completely portable. Also switched from creating hexviews for every .raw.changes file creation in favor of just performing a line-by-line binary diff, because it's way faster.

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-	savepath: '<Enter the path to your save file here, including the slot subfolder>',
+    savepath: '<Enter the path to your save file here, including the slot subfolder>',
     saveFileLastOffset: 0x000fac80,
 	placeholderImagepath: './placeholder.jpg'
 };
