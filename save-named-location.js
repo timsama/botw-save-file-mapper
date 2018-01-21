b756b6147a743baa20c88cb75915ec5efac298ca
Tim Winchester <tim_sama@hotmail.com>
2018-01-20 23:33:57 -0800

    added orientation to named locations

--- save-named-location.js
+++ save-named-location.js
@@ -1,6 +1,8 @@
 const saveNamedChange = require('./save-named-change.js');
 
 const name = process.argv[2] || readline.question('Name of location: ');
+const args = process.argv.slice(3);
+const excludeOrientation = args.indexOf('exclude-orientation') !== -1;
 
 if (!!name) {
     const offsets = [
@@ -8,6 +10,11 @@ if (!!name) {
         0x000C3BF8,
         0x000C3C00
     ];
+
+    if (!excludeOrientation) {
+        offsets.push(0x0006ea68);
+    }
+
     saveNamedChange('locations.' + name, offsets);
     console.log(`Location ${name} saved!`);
 } else {
