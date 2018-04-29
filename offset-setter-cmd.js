150214ba08ae9f599cfbfa2e076a53b860e010a2
Tim Winchester <tim_sama@hotmail.com>
2018-04-29 13:03:21 -0700

    Fixing offset setter

--- offset-setter-cmd.js
+++ offset-setter-cmd.js
@@ -1,10 +1,12 @@
-const offsetChecker = require('./offset-setter.js');
+const offsetSetter = require('./offset-setter.js');
 const saveFileUtils = require('./save-file-utils.js');
 const CONFIG = require('./config.js');
 
 const offset = parseInt(process.argv[2]);
-const value = process.argv[3];
+const valueStr = process.argv[3];
+const isValueHex = valueStr.split('').some(c => c == 'x');
+const value = isValueHex ? parseInt(valueStr, 16) : parseInt(valueStr, 10);
 const filename = process.argv[4];
 const saveFilepath = `${CONFIG.savepath}${filename || 'game_data.sav'}`;
 
-offsetChecker(offset, value, saveFilepath);
+offsetSetter(offset, value, saveFilepath);
