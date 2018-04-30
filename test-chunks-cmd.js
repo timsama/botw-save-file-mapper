8c5f401af824c506387166aa235be6ac6162c079
Tim Winchester <tim_sama@hotmail.com>
2018-04-29 20:58:16 -0700

    refactored test-chunks

---
+++ test-chunks-cmd.js
@@ -0,0 +1,65 @@
+const testChunks = require('./test-chunks.js');
+const readline = require('readline-sync');
+
+const possibleFlags = [
+    'only-ones',
+    'only-test-ones',
+    'known-value',
+    'known-previous-value',
+    'rename',
+    'include-known',
+    'include-known-offsets',
+    'append',
+    'append-to-existing'
+];
+
+const flagFilter = (str) => {
+    const [prefix] = str.split('=');
+    return possibleFlags.some(f => f == prefix);
+};
+
+const nameFilter = (str) => {
+    return !flagFilter(str);
+};
+
+const name = process.argv.slice(2).find(nameFilter) || 'unnamed';
+
+const args = process.argv.slice(2).filter(flagFilter);
+
+const renameArgs = args.filter((arg) => arg.split('=')[0] == 'rename');
+const shouldRename = renameArgs.length > 0;
+const newName = (() => {
+    if (shouldRename) {
+        const argInput = renameArgs[0].split('=')[1];
+        if (argInput) {
+            return argInput;
+        } else {
+            return readline.question('What should the new name be? ');
+        }
+    }
+})();
+
+const onlyTestOnes = args.some(arg => arg == 'only-ones' || arg == 'only-test-ones');
+const knownValueArgs = args.filter(arg => arg.split('=')[0] == 'known-value');
+const findKnownValue = knownValueArgs.length > 0;
+const knownValue = (() =>{
+    if (findKnownValue) {
+        return parseInt(knownValueArgs[0].split('=').slice(-1)[0]);
+    } else if (onlyTestOnes) {
+        return 1;
+    }
+})();
+
+const knownPreviousValueArgs = args.filter((arg) => arg.indexOf('known-previous-value=') !== -1);
+const findKnownPreviousValue = knownPreviousValueArgs.length > 0;
+const knownPreviousValue = (() =>{
+    if (findKnownPreviousValue) {
+        return parseInt(knownPreviousValueArgs[0].split('=').slice(-1)[0]);
+    }
+})();
+
+const includeKnownOffsets = args.indexOf('include-known-offsets') !== -1 || args.indexOf('include-known') !== -1;
+
+const appendToExisting = args.some(entry => entry == 'append' || entry == 'append-to-existing');
+
+testChunks(name, newName, knownValue, knownPreviousValue, !includeKnownOffsets, onlyTestOnes, undefined, undefined, undefined, undefined, undefined, appendToExisting);
--- test-chunks.js
+++ test-chunks.js
@@ -1,71 +1,73 @@
-const saveFileUtils = require('./save-file-utils.js');
-const buildRecursiveSearcher = require('./build-recursive-searcher.js');
-const resultExporter = require('./json-result-exporter.js');
-const fs = require('fs');
-const CONFIG = require('./config.js');
-const folderUtils = require('./folder-utils.js');
-const mapFileUtils = require('./map-file-utils.js');
-const query = require('cli-interact').getYesNo;
+module.exports = (name, newName, knownValue, knownPreviousValue, filterKnownOffsets, nonVariable, saveQuery, knownDependencies, queryFuncOverride, skipLogging, pathOverride, appendToExisting) => {
+    const saveFileUtils = require('./save-file-utils.js');
+    const buildRecursiveSearcher = require('./build-recursive-searcher.js');
+    const resultExporter = require('./json-result-exporter.js');
+    const fs = require('fs');
+    const CONFIG = require('./config.js');
+    const folderUtils = require('./folder-utils.js');
+    const mapFileUtils = require('./map-file-utils.js');
+    const query = require('cli-interact').getYesNo;
 
-const name = process.argv[2] || 'unnamed';
-const changesFilename = name + '.raw.changes';
-const changesFilepath = CONFIG.rawchangespath + changesFilename;
-const saveFilepath = `${CONFIG.savepath}game_data.sav`;
-const captionImagepath = `${CONFIG.savepath}caption.jpg`;
-const tempCaptionImagepath = `${CONFIG.tempoutputpath}caption.temp.jpg`;
-const args = process.argv.slice(3);
-const includeKnownOffsets = args.indexOf('include-known-offsets') !== -1 || args.indexOf('include-known') !== -1;
-const renameArgs = args.filter((arg) => arg.indexOf('rename') !== -1);
-const shouldRename = renameArgs.length > 0;
+    const changesFilename = name + '.raw.changes';
+    const changesFilepath = pathOverride || (CONFIG.rawchangespath + changesFilename);
+    const saveFilepath = `${CONFIG.savepath}game_data.sav`;
+    const captionImagepath = `${CONFIG.savepath}caption.jpg`;
+    const tempCaptionImagepath = `${CONFIG.tempoutputpath}caption.temp.jpg`;
 
-folderUtils.buildFoldersIfTheyDoNotExist(tempCaptionImagepath);
+    const args = process.argv.slice(3);
 
-const offsetFilter = (() => {
-    if (!includeKnownOffsets) {
-        return mapFileUtils.getKnownOffsetsFilter();
-    } else {
-        return () => true;
-    }
-})();
+    folderUtils.buildFoldersIfTheyDoNotExist(tempCaptionImagepath);
 
-const newName = (() => {
-    if (shouldRename) {
-        const argInput = renameArgs[0].split('=')[1];
-        if (argInput) {
-            return argInput;
+    const offsetFilter = (() => {
+        if (filterKnownOffsets) {
+            return mapFileUtils.getKnownOffsetsFilter();
         } else {
-            return readline.question('What should the new name be? ');
+            return () => true;
         }
+    })();
+
+    const unfilteredUnapplyChanges = saveFileUtils.getChangesToUnapply(changesFilepath);
+    const unfilteredApplyChanges = saveFileUtils.getChangesToApply(changesFilepath);
+    const allChangesToApply = unfilteredApplyChanges.filter((address, i) => {
+        return (knownValue === undefined || address.value === knownValue) && (knownPreviousValue === undefined || unfilteredUnapplyChanges[i].value === knownPreviousValue);
+    }).filter(offsetFilter);
+    const allChunksToApply = saveFileUtils.getChunksFromChanges(allChangesToApply);
+    const allChangesToUnapply = unfilteredUnapplyChanges.filter((address, i) => {
+        return (knownValue === undefined || unfilteredApplyChanges[i] && unfilteredApplyChanges[i].value === knownValue) && (knownPreviousValue === undefined || address.value === knownPreviousValue);
+    }).filter(offsetFilter);
+
+    const getChanges = (chunks) => {
+        return saveFileUtils.getChangesFromChunks(chunks);
+    };
+
+    if (allChunksToApply.length > 0 && allChangesToUnapply.length > 0) {
+        fs.renameSync(captionImagepath, tempCaptionImagepath);
+        fs.copyFileSync(CONFIG.placeholderImagepath, captionImagepath);
+
+        const results = saveFileUtils.withBinaryFileSync(saveFilepath, (binary) => {
+            const recursiveSearcher = buildRecursiveSearcher(saveFilepath, binary, skipLogging);
+
+            const queryFunc = !!queryFuncOverride ? queryFuncOverride : (index, allChangesLength) => {
+                return query(`Save file generated. (${index} of ${Math.ceil(Math.log(allChangesLength, 2)) + 2}) Did it work?`);
+            };
+            const results = recursiveSearcher.search(allChunksToApply, allChangesToUnapply, queryFunc, getChanges);
+
+            const mightSaveAsVariableReasons = [];
+            if (!nonVariable && (knownValue !== undefined || knownPreviousValue !== undefined)) {
+                mightSaveAsVariableReasons.push('You searched for a known value.');
+            }
+
+            resultExporter(results, newName || name, mightSaveAsVariableReasons, false, undefined, saveQuery, knownDependencies, skipLogging, appendToExisting);
+
+            return results;
+        });
+
+        fs.unlinkSync(captionImagepath);
+        fs.renameSync(tempCaptionImagepath, captionImagepath);
+
+        return results;
+    } else {
+        !skipLogging && console.log('No testable changes available with your current filter settings.');
+        return undefined;
     }
-})();
-
-const unchunkedChanges = saveFileUtils.getChangesToApply(changesFilepath).filter(offsetFilter);
-const allChunksToApply = saveFileUtils.getChunksFromChanges(unchunkedChanges);
-const allChangesToUnapply = saveFileUtils.getChangesToUnapply(changesFilepath).filter(offsetFilter);
-
-if (allChunksToApply.length > 0 && allChangesToUnapply.length > 0) {
-    fs.renameSync(captionImagepath, tempCaptionImagepath);
-    fs.copyFileSync(CONFIG.placeholderImagepath, captionImagepath);
-
-    saveFileUtils.withBinaryFileSync(saveFilepath, (binary) => {
-        const recursiveSearcher = buildRecursiveSearcher(saveFilepath, binary);
-        
-        const getChanges = (chunks) => {
-            return saveFileUtils.getChangesFromChunks(chunks);
-        };
-
-        const queryFunc = (index, allChangesLength) => {
-            query(`Save file generated. (${index} of ${Math.ceil(Math.log(allChangesLength, 2)) + 2}) Did it work?`)
-        };
-        const results = recursiveSearcher.search(allChunksToApply, allChangesToUnapply, queryFunc, getChanges);
-
-        const mightSaveAsVariableReasons = [];
-
-        resultExporter(results, newName || name, mightSaveAsVariableReasons);
-    });
-
-    fs.unlinkSync(captionImagepath);
-    fs.renameSync(tempCaptionImagepath, captionImagepath);
-} else {
-    console.log('No testable changes available with your current filter settings.');
-}
+};
