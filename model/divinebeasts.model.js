b29b1505fff3767f67e7c1a7363c7adb202e7f1c
Tim Winchester <tim_sama@hotmail.com>
2018-09-02 13:55:10 -0700

    added model for vah naboris (and some minor fixes)

--- model/divinebeasts.model.js
+++ model/divinebeasts.model.js
@@ -2,6 +2,7 @@ module.exports = (() => {
     const VahMedoh = require('./vahmedoh.model.js');
     const VahRudania = require('./vahrudania.model.js');
     const VahRuta = require('./vahruta.model.js');
+    const VahNaboris = require('./vahnaboris.model.js');
 
     return {
         read: (saveFile, effectMapPath) => {
@@ -9,12 +10,14 @@ module.exports = (() => {
                 vahmedoh: VahMedoh.read(saveFile, effectMapPath),
                 vahrudania: VahRudania.read(saveFile, effectMapPath),
                 vahruta: VahRuta.read(saveFile, effectMapPath),
+                vahnaboris: VahNaboris.read(saveFile, effectMapPath),
             };
         },
         write: (modelJson, saveFile, effectMapPath) => {
             return VahMedoh.write(modelJson.vahmedoh, saveFile, effectMapPath)
                 .then(() => VahRudania.write(modelJson.vahrudania, saveFile, effectMapPath))
                 .then(() => VahRuta.write(modelJson.vahruta, saveFile, effectMapPath))
+                .then(() => VahNaboris.write(modelJson.vahnaboris, saveFile, effectMapPath))
         }
     };
 })();
---
+++ model/vahnaboris.model.js
@@ -0,0 +1,102 @@
+module.exports = (() => {
+    const CONFIG = require('../config.js');
+    const changeReader = require('../read-changes.js');
+    const changeWriter = require('../batch-apply-changes.js');
+    const defaultEffectMap = `${CONFIG.exportpath}effectmap.json`;
+
+    const getChangeReader = (saveFile, effectMapPath) => {
+        return (keys, withLogging) => {
+            return changeReader(saveFile)(effectMapPath || defaultEffectMap, keys, withLogging);
+        };
+    };
+    const getChangeWriter = (saveFile, effectMapPath) => {
+        return (keys, skipSoftDependencies, withLogging) => {
+            return changeWriter(saveFile)(effectMapPath || defaultEffectMap, keys, skipSoftDependencies, withLogging);
+        };
+    };
+
+    return {
+        read: (saveFile, effectMapPath) => {
+            const readChanges = getChangeReader(saveFile, effectMapPath);
+
+            const mapValues = readChanges([
+                'divinebeasts.vahnaboris.active',
+                'divinebeasts.vahnaboris.complete',
+                'divinebeasts.vahnaboris.found',
+                'divinebeasts.vahnaboris.terminalsremaining',
+                'divinebeasts.vahnaboris.terminal1.on',
+                'divinebeasts.vahnaboris.terminal2.on',
+                'divinebeasts.vahnaboris.terminal3.on',
+                'divinebeasts.vahnaboris.terminal4.on',
+                'divinebeasts.vahnaboris.terminal5.on',
+                'divinebeasts.vahnaboris.heartcontainer.available',
+                'divinebeasts.vahnaboris.heartcontainer.taken'
+            ]);
+
+            return {
+                active: mapValues['divinebeasts.vahnaboris.active'],
+                complete: mapValues['divinebeasts.vahnaboris.complete'],
+                found: mapValues['divinebeasts.vahnaboris.found'],
+                terminalsremaining: mapValues['divinebeasts.vahnaboris.terminalsremaining'],
+                terminal1: mapValues['divinebeasts.vahnaboris.terminal1.on'],
+                terminal2: mapValues['divinebeasts.vahnaboris.terminal2.on'],
+                terminal3: mapValues['divinebeasts.vahnaboris.terminal3.on'],
+                terminal4: mapValues['divinebeasts.vahnaboris.terminal4.on'],
+                terminal5: mapValues['divinebeasts.vahnaboris.terminal5.on'],
+                heartcontaineravailable: mapValues['divinebeasts.vahnaboris.heartcontainer.available'],
+                heartcontainertaken: mapValues['divinebeasts.vahnaboris.heartcontainer.taken']
+            };
+        },
+        write: (modelJson, saveFile, effectMapPath) => {
+            const writeChanges = getChangeWriter(saveFile, effectMapPath);
+
+            const keys = [];
+
+            const addKeyIfTrue = (val, key) => {
+                if (val === true) {
+                    keys.push(key);
+                }
+            };
+
+            addKeyIfTrue(modelJson.active, 'divinebeasts.vahnaboris.active');
+            addKeyIfTrue(!modelJson.active, 'divinebeasts.vahnaboris.inactive');
+
+            addKeyIfTrue(modelJson.complete, 'divinebeasts.vahnaboris.complete');
+            addKeyIfTrue(!modelJson.complete, 'divinebeasts.vahnaboris.incomplete');
+
+            addKeyIfTrue(modelJson.found, 'divinebeasts.vahnaboris.found');
+            addKeyIfTrue(!modelJson.found, 'divinebeasts.vahnaboris.notfound');
+
+            let terminalsRemaining = 5;
+            modelJson.terminal1 && terminalsRemaining--;
+            modelJson.terminal2 && terminalsRemaining--;
+            modelJson.terminal3 && terminalsRemaining--;
+            modelJson.terminal4 && terminalsRemaining--;
+            modelJson.terminal5 && terminalsRemaining--;
+            keys.push(`divinebeasts.vahnaboris.terminalsremaining=${terminalsRemaining}`);
+            
+            addKeyIfTrue(modelJson.terminal1, 'divinebeasts.vahnaboris.terminal1.on');
+            addKeyIfTrue(!modelJson.terminal1, 'divinebeasts.vahnaboris.terminal1.off');
+            
+            addKeyIfTrue(modelJson.terminal2, 'divinebeasts.vahnaboris.terminal2.on');
+            addKeyIfTrue(!modelJson.terminal2, 'divinebeasts.vahnaboris.terminal2.off');
+            
+            addKeyIfTrue(modelJson.terminal3, 'divinebeasts.vahnaboris.terminal3.on');
+            addKeyIfTrue(!modelJson.terminal3, 'divinebeasts.vahnaboris.terminal3.off');
+            
+            addKeyIfTrue(modelJson.terminal4, 'divinebeasts.vahnaboris.terminal4.on');
+            addKeyIfTrue(!modelJson.terminal4, 'divinebeasts.vahnaboris.terminal4.off');
+            
+            addKeyIfTrue(modelJson.terminal5, 'divinebeasts.vahnaboris.terminal5.on');
+            addKeyIfTrue(!modelJson.terminal5, 'divinebeasts.vahnaboris.terminal5.off');
+
+            addKeyIfTrue(modelJson.heartcontaineravailable, 'divinebeasts.vahnaboris.heartcontainer.available');
+            addKeyIfTrue(!modelJson.heartcontaineravailable, 'divinebeasts.vahnaboris.heartcontainer.notavailable');
+
+            addKeyIfTrue(modelJson.heartcontainertaken, 'divinebeasts.vahnaboris.heartcontainer.taken');
+            addKeyIfTrue(!modelJson.heartcontainertaken, 'divinebeasts.vahnaboris.heartcontainer.nottaken');
+
+            return writeChanges(keys);
+        }
+    };
+})();
