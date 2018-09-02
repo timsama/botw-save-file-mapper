edc3aafbfcc87efc808647b662b9d4d0fa5eb30b
Tim Winchester <tim_sama@hotmail.com>
2018-09-02 02:11:14 -0700

    removing dead code

--- model/map-item-slots.js
+++ model/map-item-slots.js
@@ -51,39 +51,4 @@ module.exports = (() => {
 
         return items;
     };
-
-    // return (saveFile, category, func) => {
-    //     const itemsFilepath = itemFileUtils.getCategoryFilepath(category);
-    //     const itemsJson = itemFileUtils.getFileAsJsonOrEmptyJsObject(itemsFilepath);
-    //     const slotStructure = getItemSlotStructure(saveFile);
-    //     const categorySlotStructure = slotStructure[category];
-    //     const getItemName = getItemNameFromEntries(category);
-
-    //     const slots = [];
-    //     for (let slot = categorySlotStructure.first; slot <= categorySlotStructure.last; slot++) {
-    //         slots.push(slot);
-    //     }
-
-    //     const slotEntries = batchGetItemEntries(slots, saveFile);
-
-    //     const namedSlots = slotEntries.map(slotEntry => {
-    //         return {
-    //             slot: slotEntry.slot,
-    //             name: getItemName(slotEntry.entries)
-    //         };
-    //     });
-
-    //     return namedSlots.map(namedSlot => {
-    //         const slotInCategory = namedSlot.slot - categorySlotStructure.first;
-    //         if (!!namedSlot.name) {
-    //             const itemJson = itemsJson[namedSlot.name];
-    //             itemJson.name = namedSlot.name;
-
-    //             return func(itemJson, namedSlot.slot, slotInCategory);
-    //         } else {
-    //             console.log(`Item at slot ${namedSlot.slot} could not be found!`);
-    //             return undefined;
-    //         }
-    //     }).filter(item => !!item);
-    // };
 })();
