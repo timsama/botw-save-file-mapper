f17b8f46013d2fd4f7f9183e0dcf0044c2a0fa1e
Tim Winchester <tim_sama@hotmail.com>
2018-09-03 19:57:20 -0700

    updated test structure

--- test/model/runes.model.spec.js
+++ test/model/runes.model.spec.js
@@ -1,14 +1,5 @@
-module.exports = () => {
-    const assert = require('assert');
-    const Runes = require('../../model/runes.model.js');
-    const md5 = require('md5-file').sync;
-    const fs = require('fs');
-    const ModelTestUtils = require('./model-test-utils.js');
-
-    const baseFilePath = './test/blank.sav';
-    const testFilePath = './test/runes.test.sav';
-
-    const expectedFile = './test/runes.model.spec.sav';
+module.exports = (() => {
+    const subModelTest = require('./model-test-template.js');
 
     const expectedJson = {
         "selectedIndex": 1,
@@ -39,27 +30,5 @@ module.exports = () => {
         },
     };
 
-    return new Promise((resolve, reject) => {
-        describe('runes.model.js', function() {
-            after(function() {
-                if (fs.existsSync(testFilePath)) {
-                    fs.unlinkSync(testFilePath);
-                }
-            });
-
-            it('should write the runes to the save file correctly', function() {
-                fs.copyFileSync(baseFilePath, testFilePath);
-
-                return Runes.write(expectedJson, testFilePath).then(() => {
-                    // assert(md5(testFilePath) == md5(expectedFile), `${md5(testFilePath)} !== ${md5(expectedFile)}`);
-                }).then(resolve);
-            }).timeout(10000);
-
-            it('should read the runes from the save file correctly', function() {
-                const actualJson = Runes.read(testFilePath);
-
-                ModelTestUtils.deepComparison(expectedJson, actualJson, 'runes');
-            });
-        });
-    });    
-};
+    return subModelTest('runes', expectedJson);
+})();
--- test/model/savefile.model.spec.js
+++ test/model/savefile.model.spec.js
@@ -1,10 +1,5 @@
 const inventoryModelSpec = require('./inventory.model.spec.js');
-const runesModelSpec = require('./runes.model.spec.js');
 const mapModelSpec = require('./map.model.spec.js');
 
 inventoryModelSpec()
-    .then(runesModelSpec)
-    .then(() => require('./clock.model.spec.js'))
-    .then(() => require('./fairyfountains.model.spec.js'))
-    .then(() => require('./stats.model.spec.js'))
     .then(mapModelSpec)
