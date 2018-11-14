27311f94aa4fc8018c4799867e0a56c85f4e66fe
Tim Winchester <tim_sama@hotmail.com>
2018-11-13 21:31:27 -0800

    added creation of snapshots folder if it does not yet exist

--- cmd/create-blank-snapshot-cmd.js
+++ cmd/create-blank-snapshot-cmd.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 const jBinary = require('jbinary');
 const saveFileUtils = require('../util/save-file-utils.js');
+const folderUtils = require('../util/folder-utils.js');
 const CONFIG = require('../config.json');
 
 const createBlankSnapshot = (outFile) => {
     const outputFilename = `${CONFIG.snapshotspath}${outFile}.sav`;
     const inputFilename = `${CONFIG.blankpath}blank.sav`;
+
+    folderUtils.buildFoldersIfTheyDoNotExist(outputFilename);
 
     fs.copyFileSync(inputFilename, outputFilename);
 
