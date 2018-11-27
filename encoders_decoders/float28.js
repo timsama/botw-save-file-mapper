a9d07e46a4760c0ca0d8846640bd52fd4747c87b
Tim Winchester <tim_sama@hotmail.com>
2018-11-26 22:17:31 -0800

    Fixed float28.js and added output as floating point in JS

--- encoders_decoders/float28.js
+++ encoders_decoders/float28.js
@@ -1,14 +1,24 @@
 module.exports = (() => {
     return {
         encode: (intVal) => {
-            const exponent = Math.floor(Math.log2(intVal)) - 7;
-            const mantissa = intVal / (Math.pow(2, exponent) * 1.0) - 128;
-            return (((exponent + 6) << 23) | (mantissa * Math.pow(2, 16))) | 0x40000000;
+            const sign = intVal < 0 ? 0x80000000 : 0x00000000;
+            const absIntVal = Math.abs(intVal);
+            const rawExponent = Math.floor(Math.log2(absIntVal));
+            const exponent = Math.floor(Math.log2(absIntVal));
+            const mantissa = absIntVal / (Math.pow(2, rawExponent - 7) * 1.0) - 128;
+            return ((((exponent + 127) << 23) | (mantissa * Math.pow(2, 16))) | sign) >>> 0;
         },
         decode: (float28Val) => {
-            const exponent = ((float28Val & 0x0F800000) >> 23) - 6;
+            const sign = ((float28Val & 0x80000000) >> 31) * 2 + 1;
+            const exponent = ((float28Val & 0x7F800000) >> 23) - 134;
             const mantissa = (float28Val & 0x007FFFFF) / Math.pow(2, 16) + 128;
-            return Math.floor(mantissa * Math.pow(2, exponent));
+            return sign * Math.floor(mantissa * Math.pow(2, exponent));
+        },
+        decodeFloat: (float28Val) => {
+            const sign = ((float28Val & 0x80000000) >> 31) * 2 + 1;
+            const exponent = ((float28Val & 0x7F800000) >> 23) - 134;
+            const mantissa = (float28Val & 0x007FFFFF) / Math.pow(2, 16) + 128;
+            return sign * mantissa * Math.pow(2, exponent);
         }
     };
 })();
