f258f5d2777baa0a41badaf34d0fd29d27056d44
Tim Winchester <tim_sama@hotmail.com>
2018-11-27 17:04:10 -0800

    allowed for multiple decoding/encoding of floats

--- cmd/decode-float-cmd.js
+++ cmd/decode-float-cmd.js
@@ -1,6 +1,7 @@
 const float28 = require('../encoders_decoders/float28.js');
-const saveFileUtils = require('../util/save-file-utils.js');
 
-const input = process.argv[2];
+const inputs = process.argv.slice(2);
 
-console.log(float28.decode(input));
+inputs.forEach((input) => {
+    console.log(float28.decodeFloat(input));
+});
--- cmd/encode-float-cmd.js
+++ cmd/encode-float-cmd.js
@@ -1,6 +1,8 @@
 const float28 = require('../encoders_decoders/float28.js');
 const saveFileUtils = require('../util/save-file-utils.js');
 
-const input = process.argv[2];
+const inputs = process.argv.slice(2);
 
-console.log(saveFileUtils.toHexString(float28.encode(input)));
+inputs.forEach((input) => {
+    console.log(saveFileUtils.toHexString(float28.encode(input)));
+});
