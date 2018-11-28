68886db823fd21153f2ba3ad3c1f577f7ac21d3f
Tim Winchester <tim_sama@hotmail.com>
2018-11-28 05:49:04 -0800

    refactored ascii reader

--- cmd/check-ascii-cmd.js
+++ cmd/check-ascii-cmd.js
@@ -1,24 +1,7 @@
-const offsetChecker = require('../lib/offset-checker.js');
-const saveFileUtils = require('../util/save-file-utils.js');
-const CONFIG = require('../config.json');
+const asciiReader = require('../lib/read-ascii-at-offset.js')();
+
 
 const offset = parseInt(process.argv[2]);
 const length = parseInt(process.argv[3]);
-const offsets = Array.apply(0, new Array(length)).map((e, i) => i * 8 + offset);
-const saveFilepath = `${CONFIG.savepath}game_data.sav`;
 
-const values = offsets.map((offset) => offsetChecker(offset, saveFilepath));
-const hexStrings = values.map((value) => saveFileUtils.toHexString(value));
-const codes = [];
-hexStrings.forEach((hexString) => {
-    hexString.split('').reduce((prev, next) => {
-        if (prev && next) {
-            codes.push(`0x${prev}${next}`);
-            return undefined;
-        } else {
-            return next;
-        }
-    }, undefined)
-});
-const chars = codes.map((code) => String.fromCharCode(code));
-console.log(chars.join(''));
+console.log(asciiReader(offset, length));
