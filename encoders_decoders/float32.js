a79d90cadd2c54b47cec44a837fb5446250933e9
Tim Winchester <tim_sama@hotmail.com>
2018-11-28 20:12:04 -0800

    fixes for float32 when exponent bits are all 0

--- encoders_decoders/float32.js
+++ encoders_decoders/float32.js
@@ -16,8 +16,10 @@ module.exports = (() => {
         },
         decode: (float32Val) => {
             const sign = ((float32Val & 0x80000000) >> 31) * 2 + 1;
-            const exponent = ((float32Val & 0x7F800000) >> 23) - 134;
-            const mantissa = (float32Val & 0x007FFFFF) / Math.pow(2, 16) + 128;
+            const exponent = ((float32Val & 0x7F800000) >> 23) - 127;
+            const useImplicitLeadingOne = exponent !== -127;
+            const leadingOne = useImplicitLeadingOne ? 0x00800000 : 0x00000000;
+            const mantissa = (float32Val & 0x007FFFFF | leadingOne) / Math.pow(2, 23);
             return sign * mantissa * Math.pow(2, exponent);
         }
     };
